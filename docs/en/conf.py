376597336b2dc4c8db2e0b40f3138c8013c4a4a3
cpot <cpotter@ceo-vision.com>
2018-11-06 21:23:07 +0100

    Create conf.py

---
+++ docs/en/conf.py
@@ -0,0 +1,17 @@
+import os, sys, subprocess
+
+CWD = os.path.dirname(os.path.realpath(__file__))
+PROJECT_PATH = os.path.dirname(os.path.realpath('%s/..' % __file__))
+
+sys.path.append(PROJECT_PATH)
+
+from conf import *
+
+latex_documents = [
+  ('index', 'SignavioWorkflowUserGuide.tex', u"Le guide de l'utilisateur Signavio Workflow Accelerator",
+   ", ".join(AUTHORS), 'manual'),
+]
+
+language = 'en'
+
+prepare(CWD, PROJECT_PATH, language)
