5f002ecf8816a130345053d9a482dea2335443bc
cpot <cpotter@ceo-vision.com>
2018-11-06 20:51:56 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -130,7 +130,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'effektif_sphinx_theme'
+#html_theme = 'effektif_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -138,7 +138,7 @@ html_theme = 'effektif_sphinx_theme'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [effektif_sphinx_theme.get_html_theme_path()]
+#html_theme_path = [effektif_sphinx_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
