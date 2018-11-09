674798d9829df91f4e8471df8acd8505fd6720ea
cpot <cpotter@ceo-vision.com>
2018-11-09 22:11:05 +0100

    Update gettext.sh

--- gettext.sh
+++ gettext.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
-pip install sphinx-intl
-sphinx-intl update -p _build/gettext -l fr_FR
+
+if [ ! $1 ]
+then
+    echo "No language has been specified!"
+
+    exit 1
+fi
+
+python cleanup.py $1
+
+cd docs
+make clean > /dev/null
+make gettext 
+#> /dev/null
+sphinx-intl update -p _build/locale -l $1 
+#> /dev/null
+sphinx-intl build 
+#> /dev/null
